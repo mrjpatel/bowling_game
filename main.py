10ea5eeec797ea265cace4634bb5e9985915d197
Japan Patel <19561816+mrjpatel@users.noreply.github.com>
2021-06-27 15:16:09 +1000

    added a driver class

---
+++ main.py
@@ -0,0 +1,26 @@
+from bowling_game import BowlingGame
+
+
+def main():
+    bg = BowlingGame()
+    bg.roll(4)
+    bg.roll(4)
+    bg.print_scores()
+
+    bg = BowlingGame()
+    bg.roll(4)
+    bg.roll(6)  # spare
+    bg.roll(5)
+    bg.roll(0)
+    bg.print_scores()
+
+    bg = BowlingGame()
+    bg.roll(10)  # strike
+    bg.roll(5)
+    bg.roll(4)
+    bg.roll(0)
+    bg.print_scores()
+
+
+if __name__ == "__main__":
+    main()
